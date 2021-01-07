4264143e6da599cf9017cd7a3d0488d3ad7fe430
chunky <dedb3at@gmail.com>
2021-01-07 15:45:24 +0100

    added all files from personal pc

---
+++ File Sort/FileSorter.cs
@@ -0,0 +1,81 @@
+﻿using File_Sort;
+using System;
+using System.IO;
+
+namespace FileSort
+{
+    class FileSorter
+    {
+        public static void Sort(string path, int choice)
+        {
+            if (choice == 1)
+            {
+                SortFolderByName(path);
+            }
+            else if (choice == 2)
+            {
+                SortFolderByType(path);
+            }
+
+        }
+
+        private static void SortFolderByName(string path)
+        {
+            string parentPath, destination, fileName, fileType;
+
+            foreach (var a in Directory.GetFiles(path))
+            {
+                parentPath = Path.GetDirectoryName(a);
+
+                destination = Path.GetFileName(a);
+                destination = destination.Substring(0, destination.LastIndexOf("."));
+                destination = destination.Substring(0, destination.LastIndexOf(".")).Replace(".", @"\");
+
+                fileName = Path.GetFileNameWithoutExtension(a);
+                fileName = fileName.Substring(fileName.LastIndexOf(".") + 1);
+
+                fileType = a.Substring(a.LastIndexOf(".") + 1);
+
+                MoveFile(parentPath, destination, fileName, fileType, a);
+            }
+        }
+        private static void SortFolderByType(string path)
+        {
+            string parentPath, fileName, fileType;
+
+            foreach (var a in Directory.GetFiles(path))
+            {
+                parentPath = Path.GetDirectoryName(a);
+
+                fileName = Path.GetFileNameWithoutExtension(a);
+                fileName = fileName.Substring(fileName.LastIndexOf(".") + 1);
+
+                fileType = a.Substring(a.LastIndexOf(".") + 1);
+
+                MoveFile(parentPath, fileType, fileName, fileType, a);
+            }
+        }
+
+
+        private static void MoveFile(string parentPath, string destination, string fileName, string fileType, string pathOfCurrentFile)
+        {
+
+            try
+            {
+                Directory.Move(pathOfCurrentFile, $@"{parentPath}\{destination}\{fileName}.{fileType}");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                if (MainWindow.DirectoryNotFoundChoice(fileName) == "Yes")
+                {
+                    Directory.CreateDirectory($@"{parentPath}\{destination}");
+                    Directory.Move(pathOfCurrentFile, $@"{parentPath}\{destination}\{fileName}.{fileType}");
+                }
+                else
+                {
+                    //do nothing
+                }
+            }
+        }
+    }
+}
---
+++ File Sort/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using FileSort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace File_Sort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_beginSort_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtBox_folderPath.Text.Length == 0 || cmbBox_sortingType.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please make sure you have entered your Folder Path and selected a Sorting Method");
+            }
+            else
+            {
+                FileSorter.Sort(txtBox_folderPath.Text, cmbBox_sortingType.SelectedIndex);
+            }
+            MessageBox.Show("All files sorted", "Success", MessageBoxButton.OK);
+        }
+
+        public static string DirectoryNotFoundChoice(string fileName)
+        {
+            if (MessageBox.Show($"Not all required folders currently exist to sort {fileName}\nWould you like to create the required folders to sort this file?\nIf not this file will not be sorted."
+                    , "Error", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void txtBox_destinationFolder_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void cmbBox_sortingType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+    }
+}
